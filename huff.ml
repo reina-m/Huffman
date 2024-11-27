51f4c72596461baaf6de741a3ce0524b0cdce9c7
Reina AL MASRI <reina@client-172-18-73-160.eduroam.universite-paris-saclay.fr>
2024-11-27 17:08:38 +0100

    Partie : Réflexions Initiales

--- huff.ml
+++ huff.ml
@@ -1,4 +1,8 @@
-let main () = Huffman.decompress "fichier"
+let main () = 
+  let in_c = open_in Sys.argv.(1) in 
+  let tab = Huffman.char_freq in_c in 
+  Array.iteri (fun i v -> Printf.printf "Octet %d : %d occurences\n" i v) tab
 
+  (* Huffman.decompress "fichier";*)
 
 let () = main ()
--- huffman.ml
+++ huffman.ml
@@ -1,2 +1,12 @@
 let decompress _ = failwith "todo"
 let compress _ = failwith "todo"
+
+let rec char_freq in_c = 
+  let tab = Array.make 256 0 in
+    try 
+      let o = input_byte in_c in 
+      tab.(o) <- tab.(o) + 1;
+      char_freq in_c
+    with 
+    | End_of_file -> tab
+;;
