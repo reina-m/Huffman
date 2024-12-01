f327399e77d5107f7605d9e65fd7ada4d3d8ef6d
Reina AL MASRI <reina@Reinas-MacBook-Air.local>
2024-12-01 12:16:48 +0100

    Premières fonctions sur heap. Les fonctions sont à revoir et à tester.

--- heap.ml
+++ heap.ml
@@ -2,9 +2,32 @@ type 'a t = 'a list (* remplacer par une définition qui vous convient *)
 
 let empty = []
 
-let is_singleton _ = failwith "todo"
-let is_empty _ = failwith "todo"
-let add _ _ = failwith "todo"
-let find_min _ = failwith "todo"
+let is_singleton t = 
+  match t with 
+  | [_] -> true 
+  | _ -> false 
+;;
 
-let remove_min _ = failwith "todo"
+let is_empty t = 
+  t = empty (*à tester*)
+;;
+
+let rec add t x = 
+  match t with 
+  | [] -> [x]
+  | e :: tt ->  
+    if x <= e then x :: e :: tt
+    else e :: add tt x
+;;
+
+let find_min t = 
+  match t with 
+  | [] -> failwith "find_min on empty heap"
+  | e :: _ -> e (*car c'est un tas?*)
+;;
+
+let remove_min t = 
+  match t with 
+  | [] -> failwith "remove_min on empty heap"
+  | e :: tt -> (e, tt)
+;;
