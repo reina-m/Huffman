ad0c831590a7b320cf4404f68c39dded0e68fea5
Reina AL MASRI <reina@Reinas-MacBook-Air.local>
2024-12-01 15:00:22 +0100

    Bugfix : réinitalisation de tab à chaque appel récursif

--- huff.ml
+++ huff.ml
@@ -1,7 +1,7 @@
 let main () = 
   let in_c = open_in Sys.argv.(1) in 
   let tab = Huffman.char_freq in_c in 
-  Array.iteri (fun i v -> Printf.printf "Octet %d : %d occurences\n" i v) tab
+  Array.iteri (fun i v -> if v > 0 then Printf.printf "Octet %d : %d occurences\n" i v) tab
 
   (* Huffman.decompress "fichier";*)
 
--- huffman.ml
+++ huffman.ml
@@ -1,12 +1,15 @@
 let decompress _ = failwith "todo"
 let compress _ = failwith "todo"
 
-let rec char_freq in_c = 
+let char_freq in_c = 
   let tab = Array.make 256 0 in
-    try 
-      let o = input_byte in_c in 
-      tab.(o) <- tab.(o) + 1;
-      char_freq in_c
-    with 
-    | End_of_file -> tab
+  let rec aux () = 
+      try 
+        let o = input_byte in_c in 
+        tab.(o) <- tab.(o) + 1;
+        aux ()
+      with 
+      | End_of_file -> tab
+    in
+    aux ()
 ;;
