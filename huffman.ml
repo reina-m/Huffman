dbe31c4b70ee9595108deb03c731d3bbbe3f2fd8
Reina AL MASRI <reina@client-172-18-120-222.eduroam.universite-paris-saclay.fr>
2024-12-04 14:07:44 +0100

    Remarque de la séance 1

--- huffman.ml
+++ huffman.ml
@@ -1,15 +1,23 @@
 let decompress _ = failwith "todo"
 let compress _ = failwith "todo"
 
+let input_code cin = 
+  (*fonction qui gère l'exception*)
+  try 
+    input_byte cin 
+  with End_of_file -> -1
+;;
+
+
 let char_freq in_c = 
   let tab = Array.make 256 0 in
   let rec aux () = 
-      try 
-        let o = input_byte in_c in 
-        tab.(o) <- tab.(o) + 1;
-        aux ()
-      with 
-      | End_of_file -> tab
-    in
-    aux ()
+        let o = input_code in_c in 
+          if o < 0 then tab
+          else (
+            tab.(o) <- tab.(o) + 1;
+            aux ()
+          )
+  in
+  aux ()
 ;;
