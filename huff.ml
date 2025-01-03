d57d30d550f0d0bb34b84f55b3b8db3cf09f1c5c
Mgueye <maram-sall.gueye@etu-upsaclay.fr>
2025-01-03 15:00:45 +0100

    test sur une nouvelle de Poe (test non concluant) + le main (peut-etre pas important)

--- huff.ml
+++ huff.ml
@@ -1,8 +1,38 @@
-let main () = 
-  let in_c = open_in Sys.argv.(1) in 
-  let tab = Huffman.char_freq in_c in 
-  Array.iteri (fun i v -> if v > 0 then Printf.printf "Octet %d : %d occurences\n" i v) tab
+let main () =
+  if Array.length Sys.argv < 2 then (
+    Huffman.help ();
+    exit 1
+  ) else
+    match Sys.argv.(1) with
+    | "--help" -> Huffman.help ()
+    | "--stats" ->
+      if Array.length Sys.argv < 3 then (
+        Printf.eprintf "Erreur : fichier non spécifié pour --stats\n";
+        exit 1
+      ) else if not (Sys.file_exists Sys.argv.(2)) then (
+        Printf.eprintf "Erreur : fichier %s introuvable\n" Sys.argv.(2);
+        exit 1
+      ) else
+        Huffman.stats Sys.argv.(2)
+    | file when Filename.check_suffix file ".hf" ->
+      if not (Sys.file_exists file) then (
+        Printf.eprintf "Erreur : fichier %s introuvable\n" file;
+        exit 1
+      ) else (
+        Printf.printf "Décompression du fichier %s\n" file;
+        try Huffman.decompress file
+        with e -> Printf.eprintf "Erreur de décompression : %s\n" (Printexc.to_string e)
+      )
+    | file ->
+      if not (Sys.file_exists file) then (
+        Printf.eprintf "Erreur : fichier %s introuvable\n" file;
+        exit 1
+      ) else (
+        Printf.printf "Compression du fichier %s\n" file;
+        try Huffman.compress file
+        with e -> Printf.eprintf "Erreur de compression : %s\n" (Printexc.to_string e)
+      )
+;;
 
-  (* Huffman.decompress "fichier";*)
 
 let () = main ()
