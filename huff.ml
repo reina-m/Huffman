7c757ecceb4021d4f1c868bd280fc174a0a307ad
Mgueye <maram-sall.gueye@etu-upsaclay.fr>
2025-01-18 10:41:52 +0100

    avec open Huffman

--- huff.ml
+++ huff.ml
@@ -1,15 +1,20 @@
-let main () =
-  match Sys.argv with
-  | [| _; "--help" |] -> print_help ()
-  | [| _; fichier |] when Filename.check_suffix fichier ".hf" -> decompress fichier
-  | [| _; fichier |] ->
-      Printf.printf "Compression du fichier : %s\n" fichier;
-      compress fichier
-  | [| _; "--stats"; fichier |] -> stats fichier
-  | _ ->
-      Printf.printf "Fichier introuvable.\n";
-      print_help ()
-;;
+open Huffman
 
-(* Lancer le programme *)
+let main () =
+  let argc = Array.length Sys.argv in
+  if argc = 2 then
+    let arg = Sys.argv.(1) in
+    if arg = "--help" then help ()
+    else if Filename.check_suffix arg ".hf" then decompress arg
+    else (
+      Printf.printf "Compression du fichier : %s\n" arg;
+      compress arg
+    )
+  else if argc = 3 && Sys.argv.(1) = "--stats" then
+    stats Sys.argv.(2)
+  else (
+    Printf.printf "Usage pas bon. Utilisez '--help' pour plus avoir plus d'information ;)\n";
+    help ()
+  )
+;;
 let () = main ()
