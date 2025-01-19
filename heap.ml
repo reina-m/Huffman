ba6ababbb34cd10fe308f97b7a7a3223d4567a1f
Mgueye <maram-sall.gueye@etu-upsaclay.fr>
2025-01-19 06:10:18 +0100

    commit pour le reste des changements dans heap.ml

--- heap.ml
+++ heap.ml
@@ -37,14 +37,7 @@ let rec remonte_heap h i =
 
 let rec add x h = 
   let h' = h @ [x] in 
-  let rec aux heap i = 
-    if i < List.length heap then
-      let h2 = remonte_heap heap i in
-      aux h2 (i + 1)
-    else 
-      heap
-  in
-  aux h' (List.length h')
+  remonte_heap h' (List.length h)
 
 let find_min h = 
   match h with 
@@ -52,9 +45,34 @@ let find_min h =
   | e :: _ -> e
 ;;
 
+let rec descend_heap h i =
+  let sag = 2 * i + 1 in  
+  let sad = 2 * i + 2 in  
+  let len = List.length h in
+
+  let min_index =
+    if sag < len && fst (List.nth h sag) < fst (List.nth h i) then sag else i
+  in
+  let min_index =
+    if sad < len && fst (List.nth h sad) < fst (List.nth h min_index) then sad else min_index
+  in
+
+  if min_index <> i then
+    let tmp = List.nth h i in
+    let h' = List.mapi (fun idx elem ->
+      if idx = i then List.nth h min_index
+      else if idx = min_index then tmp
+      else elem
+    ) h in
+    descend_heap h' min_index
+  else h
+
+
 let remove_min h = 
   match h with 
   | [] -> failwith "remove_min on empty heap"
-  | e :: hh -> (e, hh)
-;;
-
+  | [e] -> (e, [])  
+  | e :: hh -> 
+    let dernier_elem = List.nth h (List.length h - 1) in
+    let h1 = dernier_elem :: (List.tl hh) in
+    (e, descend_heap h1 0)
