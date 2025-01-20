5dd55ea95961dd4de1f59dee6bd8efd2f41d0033
Mgueye <maram-sall.gueye@etu-upsaclay.fr>
2025-01-20 08:15:07 +0100

    version Array

---
+++ Arrayhuff.ml
@@ -0,0 +1,20 @@
+open Arrayhuffman
+
+let main () =
+  let argc = Array.length Sys.argv in
+  if argc = 2 then
+    let arg = Sys.argv.(1) in
+    if arg = "--help" then help ()
+    else if Filename.check_suffix arg ".hf" then decompress arg
+    else (
+      Printf.printf "Compression du fichier : %s\n" arg;
+      compress arg
+    )
+  else if argc = 3 && Sys.argv.(1) = "--stats" then
+    stats Sys.argv.(2)
+  else (
+    Printf.printf "Usage pas bon. Utilisez '--help' pour plus avoir plus d'information ;)\n";
+    help ()
+  )
+;;
+let () = main ()
